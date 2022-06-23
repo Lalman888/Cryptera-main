99fec2ff91b73a106c9b3fc3f521d2398f0c1765
Mann-Credencesoft <mannpatel@credencesoft.in>
2022-06-23 17:11:10 +0530

    nav bar and landing page final

--- js/button.js
+++ js/button.js
@@ -8,3 +8,14 @@ jQuery(document).ready(function(){
     if(jQuery("#uc_super_fun_liquid_button_elementor39409").length) uc_super_fun_liquid_button_elementor39409_start(); else
     jQuery( document ).on( 'elementor/popup/show', () => { if(jQuery("#uc_super_fun_liquid_button_elementor39409").length) uc_super_fun_liquid_button_elementor39409_start();});
 });
+
+jQuery(document).ready(function(){
+    function uc_super_fun_liquid_button_elementor39408_start(){
+        jQuery(document).ready(function(){
+            var objButton = document.getElementById("uc_super_fun_liquid_button_elementor39408");
+            objButton.liquidButton = new LiquidButton(objButton);
+        });
+    }
+    if(jQuery("#uc_super_fun_liquid_button_elementor39408").length) uc_super_fun_liquid_button_elementor39408_start(); else
+    jQuery( document ).on( 'elementor/popup/show', () => { if(jQuery("#uc_super_fun_liquid_button_elementor39408").length) uc_super_fun_liquid_button_elementor39408_start();});
+});
