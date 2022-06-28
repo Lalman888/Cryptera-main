b3829baf91c44f40ea20a0e4762690eae7e39044
Aarjav Hansoti <56119008+aarjavh@users.noreply.github.com>
2022-06-29 01:06:57 +0530

    first commit

---
+++ coming soon/script.js
@@ -0,0 +1,10 @@
+$("#typed").typed({
+  strings: ["Coming Soon", "Join our discord to get notified"],
+  typeSpeed: 20,
+  startDelay: 0,
+  backSpeed: 20,
+  backDelay: 1000,
+  loop: true,
+  cursorChar: "|",
+  contentType: "html"
+});
