9c8479c481642c59bd4fafbc9f0784f2ebac4ad0
Mann-Credencesoft <mannpatel@credencesoft.in>
2022-07-31 17:13:47 +0530

    FINAL FINAL

---
+++ coming soon/payscript.js
@@ -0,0 +1,10 @@
+$("#typed").typed({
+    strings: ["Successful Payment", "Our Team will get in Touch with you"],
+    typeSpeed: 20,
+    startDelay: 0,
+    backSpeed: 20,
+    backDelay: 1000,
+    loop: true,
+    cursorChar: "|",
+    contentType: "html"
+  });
--- js/button.js
+++ js/button.js
@@ -19,3 +19,13 @@ jQuery(document).ready(function(){
     if(jQuery("#uc_super_fun_liquid_button_elementor39408").length) uc_super_fun_liquid_button_elementor39408_start(); else
     jQuery( document ).on( 'elementor/popup/show', () => { if(jQuery("#uc_super_fun_liquid_button_elementor39408").length) uc_super_fun_liquid_button_elementor39408_start();});
 });
+jQuery(document).ready(function(){
+    function uc_super_fun_liquid_button_elementor39407_start(){
+        jQuery(document).ready(function(){
+            var objButton = document.getElementById("uc_super_fun_liquid_button_elementor39407");
+            objButton.liquidButton = new LiquidButton(objButton);
+        });
+    }
+    if(jQuery("#uc_super_fun_liquid_button_elementor39407").length) uc_super_fun_liquid_button_elementor39407_start(); else
+    jQuery( document ).on( 'elementor/popup/show', () => { if(jQuery("#uc_super_fun_liquid_button_elementor39407").length) uc_super_fun_liquid_button_elementor39407_start();});
+});
